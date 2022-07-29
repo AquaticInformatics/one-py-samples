e08d869bb7273dadbcf2871c1fb71e9c9e3c87c9
Dan <daniel.edwards@aquaticinformatics.com>
2022-07-29 09:11:27 -0600

    adding comments to library sample

--- librarySample.py
+++ librarySample.py
@@ -3,8 +3,9 @@ from one_py_sdk.shared.constants import Environment
 
 client =ClientSdk(cacheTimeout=50)#creates client targeting production with 50 second in memory cache for REST requests 
 client.Authentication.GetToken("userName", "password")
-units =client.Library.GetUnits()
-i18n=client.Library.Geti18nKeys("AQI_FOUNDATION_LIBRARY")
-quantityTypes =client.Library.GetQuantityTypes()
-parameters =client.Library.GetParameters()
+units =client.Library.GetUnits()#Gets all units
+i18n=client.Library.Geti18nKeys("AQI_FOUNDATION_LIBRARY", "en") #gets I18N information for particular module for particular language
+quantityTypes =client.Library.GetQuantityTypes() #Gets all quantity types
+parameters =client.Library.GetParameters() #gets all parameters
+
 print(units, i18n, quantityTypes, parameters)
