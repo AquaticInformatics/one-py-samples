60e58c771c96b861a707dc1ee4769157d7f3d586
Dan <daniel.edwards@aquaticinformatics.com>
2023-01-12 16:21:09 -0700

    report sample

---
+++ reportSample.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta, timezone
+from one_py_sdk.clientsdk import ClientSdk
+from one_py_sdk.shared.constants import Environment 
+from creds import *
+#client =ClientSdk() #Use for production environment with no cache
+client =ClientSdk(Environment.get('feature'), 50) #Use for feature environment with 50 second in memory cache of API requests
+#client =ClientSdk(Environment.get('integration')) #Use for integration environment no cache
+#client =ClientSdk(Environment.get('stage')) #Use for stage environment no cache
+#client =ClientSdk(cacheTimeout=30) Use for production with 30 second in memory cache of requests
+
+if (client.Authentication.GetToken(userName, password)):  # Supply username and password here
+    print("Authenticated successfully")
+else:
+    print("Authentication failed")
+reportDefs =client.Report.GetReportDefinitions(plantId)
+
+reportNamesAndIds = [[report.id, report.name.value] for report in reportDefs]
+lstById = []
+lstByName = []
+nameId=reportNamesAndIds[0]
+reportDefsForPlant = client.Report.GetReportDefinitions(plantId)    
+reportDef = client.Report.GetReportDefinitionById(nameId[0])    
+colIdsUsingReportId =client.Report.GetColumnsIdsByReportId(nameId[0])
+colIdsUsingName = client.Report.GetColumnsIdsByReportName(nameId[1], plantId)
+colIdsForWholePlant =client.Report.GetReportColumnsIdsByPlant(plantId)
+print(f"Columns in Report by name: \n {colIdsUsingName}")
+print(f"Columns in Report by Id: \n {colIdsUsingReportId}")
+print(f"Columns in Reports by plant: \n {colIdsForWholePlant}")
+print (f"Reports by plant: \n {reportDefsForPlant}")
+print (f"Report retrieved by Id: \n {reportDef}")
